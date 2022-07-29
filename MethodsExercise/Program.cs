8059a6d480c500a9c1e0260873bd4cf78af3cdb7
Tetyana Murzina <tetyanamur@gmail.com>
2022-07-28 22:05:04 -0500

    initial commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -54,51 +54,7 @@ namespace MethodsExercise
 
 
 
-namespace pract
-{
-    class Program
-    {
 
-        public static int Addition(int num1, int num2, int num3)
-        {
-            return num1 + num2 + num3;
-        }
-        public static int Subtraction(int num1, int num2, int num3)
-        {
-            return num1 - num2 - num3;
-        }
-        public static int Multiply(int num1, int num2, int num3)
-        {
-            return num1 * num2 * num3;
-        }
-        public static int Divide(int num1, int num2, int num3)
-        {
-            return num1 / num2 / num3;
-        }
-        public static int Modulus(int num1, int num2)
-        {
-            return (num1 % num2);
-        }
-        static void Main(string[] args)
-        {
-            int answer1 = Addition(5, 4, 7);
-            Console.WriteLine(answer1);
-
-            int answer2 = Subtraction(10, 5, 2);
-            Console.WriteLine(answer2);
-
-            int answer3 = Multiply(8, 7, 3);
-            Console.WriteLine(answer3);
-
-            int answer4 = Divide(40, 5, 4);
-            Console.WriteLine(answer4);
-
-            int answer5 = Modulus(answer4 - 1, 5);
-            Console.WriteLine(answer5);
-
-        }
-    }
-}
 
 
 
